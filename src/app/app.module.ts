b822dd891cb3ebde7bdcac048224bd9c8f1ca26f
William Cahill <drewlikesramen@gmail.com>
2020-09-09 21:16:02 -0400

    Created basic template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatButtonModule } from '@angular/material/button'
+
+import { HeaderComponent } from './header/header/header.component';
+import { RecipesComponent } from './recipes/recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail/recipe-detail.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent
   ],
   imports: [
+    MatGridListModule,
+    MatToolbarModule,
+    MatButtonModule,
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
